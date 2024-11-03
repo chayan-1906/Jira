cbaa721186499c80188e87bd421e9e69c6fd64ce
Padmanabha Das <padmanabhadas9647@gmail.com>
2024-11-03 20:38:31 +0530

    Customized Shadcn UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
 
 function Home() {
     return (
-        <div className={'flex h-full justify-center items-center bg-red-400 gap-3 p-3'}>
-            <p className={'font-black'}>Home Page</p>
-            <Button variant={'outline'}>Hello</Button>
+        <div className={'flex flex-col gap-3 p-3'}>
+            <div className={'flex h-full justify-center items-center gap-3 p-3'}>
+                <Button variant={'primary'} size={'lg'}>Primary</Button>
+                <Button variant={'secondary'}>Secondary</Button>
+                <Button variant={'destructive'} size={'sm'}>Destructive</Button>
+                <Button variant={'ghost'}>Ghost</Button>
+                <Button variant={'muted'} size={'xs'}>Muted</Button>
+                <Button variant={'outline'} disabled>Outline</Button>
+                <Button variant={'tertiary'}>Tertiary</Button>
+            </div>
+            <Input/>
         </div>
     );
 }
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,15 +1,10 @@
 "use client"
 
 import * as React from "react"
-import {
-  CaretSortIcon,
-  CheckIcon,
-  ChevronDownIcon,
-  ChevronUpIcon,
-} from "@radix-ui/react-icons"
+import {CaretSortIcon, CheckIcon, ChevronDownIcon, ChevronUpIcon,} from "@radix-ui/react-icons"
 import * as SelectPrimitive from "@radix-ui/react-select"
 
-import { cn } from "@/lib/utils"
+import {cn} from "@/lib/utils"
 
 const Select = SelectPrimitive.Root
 
@@ -24,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-12 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
